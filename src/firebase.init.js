5d353db44d001fe353b5ab19f73e0a69c1bfdf6c
Nishadchowdhury <Nishadhj111@gmail.com>
2023-09-08 00:15:14 +0600

    message

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,18 +1,17 @@
-
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCVpdRY2uPUdtUZ99fL5D71uBsDQ7Pv4do",
-  authDomain: "assignment-10-3a9c6.firebaseapp.com",
-  projectId: "assignment-10-3a9c6",
-  storageBucket: "assignment-10-3a9c6.appspot.com",
-  messagingSenderId: "891547264636",
-  appId: "1:891547264636:web:579e50a36e1eabd378cbf9"
+  apiKey: "AIzaSyDorykngAsWD0m2ubhGBcP40nzsmAJJV8w",
+  authDomain: "sisha-master.firebaseapp.com",
+  projectId: "sisha-master",
+  storageBucket: "sisha-master.appspot.com",
+  messagingSenderId: "73720856571",
+  appId: "1:73720856571:web:d1cf439807a6487dad155e",
 };
 
 // Initialize Firebase
